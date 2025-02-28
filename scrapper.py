27c49a285ff193576a97f77fa0c97a00f0686cee
Ayush <parekh56@uwindsor.ca>
2025-02-28 12:23:35 -0500

    windsor, university keyword search

--- scrapper.py
+++ scrapper.py
@@ -47,7 +47,7 @@ logging.basicConfig(
 
 # Constants
 BASE_DOMAIN = "remembering.ca"
-SEARCH_KEYWORD = "Windsor"
+SEARCH_KEYWORD = "Windsor", "University"
 ALUMNI_KEYWORDS = {"University of Windsor", "UWindsor", "Windsor University"}
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
